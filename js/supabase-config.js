cda805ae3db030359d3c0662192defbf76689b6c
gnltalkai <guysonio2009@gmail.com>
2026-09-06 17:42:55 +0100

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://whbiobcfavdzzqgeziqs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_K6sw1wGbGrWbVkFizbPBDA_F0F3XB0B";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
